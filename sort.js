9efb63bcf34a6d4516f8e06d26d443351b9b19d7
suzu-0101 <06rinach@gmail.com>
2026-05-09 17:31:21 +0900

    Remove sorting logic and add main branch message
    Removed sort function and added a console log message.

--- sort.js
+++ sort.js
@@ -1,12 +1,1 @@
-var sortNumber = function (number) {
-   number.sort(function (a, b) {
-       if (a === b) {
-           return 0;
-       }
-       return "conflict!";
-   });
-};
-
-var number = [19, 3, 81, 1, 24, 21];
-sortNumber(number);
-console.log(number);
+console.log("This is MAIN branch");
